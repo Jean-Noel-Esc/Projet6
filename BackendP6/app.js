784cc5b28b25ff7dbac1239d2e45d91e3da7225c
Jean-Noel-Esc <60701865+Jean-Noel-Esc@users.noreply.github.com>
2021-01-27 07:38:47 +0100

    Commit du 27-1 MVC
    MVC Ok routes, controllers, models, users, schémas de données, multer, fs instal ok. Implémentation du CRUD 90% (sauf fct delete still in progress).

---
+++ BackendP6/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express()
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb+srv://1hKx79cGCAIxuI8Q:1hKx79cGCAIxuI8Q@cluster0.ercbq.mongodb.net/Mydatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+const saucesRoutes = require('./routes/sauces');
+
+const userRoutes = require('./routes/user');
+
+const path = require('path');
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+
+
+
+
+
+module.exports = app;
+
